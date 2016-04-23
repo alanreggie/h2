919b6433e3cdcb8ce3a08b9e74a7242b5d4b5605
Alan Michaan <almichaan@gmail.com>
2016-04-22 20:12:45 -0400

    beginning integration

---
+++ public/js/controllers/table.js
@@ -0,0 +1,112 @@
+materialAdmin
+    .controller('tableCtrl', function($filter, $sce, ngTableParams, tableService, $scope, $http) {
+        var data = tableService.data;
+        
+        //Basic Example
+        this.tableBasic = new ngTableParams({
+            page: 1,            // show first page
+            count: 10           // count per page
+        }, {
+            total: data.length, // length of data
+            getData: function ($defer, params) {
+                $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+            }
+        })
+        
+        //Sorting
+        this.tableSorting = new ngTableParams({
+            page: 1,            // show first page
+            count: 10,           // count per page
+            sorting: {
+                name: 'asc'     // initial sorting
+            }
+        }, {
+            total: data.length, // length of data
+            getData: function($defer, params) {
+                // use build-in angular filter
+                var orderedData = params.sorting() ? $filter('orderBy')(data, params.orderBy()) : data;
+    
+                $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+            }
+        })
+        
+        //Filtering
+        this.tableFilter = new ngTableParams({
+            page: 1,            // show first page
+            count: 10
+        }, {
+            total: data.length, // length of data
+            getData: function($defer, params) {
+                // use build-in angular filter
+                var orderedData = params.filter() ? $filter('filter')(data, params.filter()) : data;
+
+                this.id = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
+                this.name = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
+                this.email = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
+                this.username = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
+                this.contact = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
+
+                params.total(orderedData.length); // set total for recalc pagination
+                $defer.resolve(this.id, this.name, this.email, this.username, this.contact);
+            }
+        })
+        
+        //Editable
+        this.tableEdit = new ngTableParams({
+            page: 1,            // show first page
+            count: 10           // count per page
+        }, {
+            total: data.length, // length of data
+            getData: function($defer, params) {
+                $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+            }
+        });
+    
+
+        //this.init = function(){
+            //alert('test')
+       // };
+
+       this.getAll = function(){
+            
+
+        $http({
+              method: 'GET',
+              url: 'http://localhost:3000/unAuthenticatedUsersAdmin',
+              data: {
+                  /*'Email': $('#registerEmail').val()*/
+              }
+        })
+        .then(function(response){
+             console.log(response.data)
+
+           var users = response.data;
+
+           //function AngularWayCtrl($resource) {
+              /*var vm = this;
+              $resource(users).query().$promise.then(function(users) {
+                  vm.users = users;
+              });*/
+            //}
+           $scope.users = users;
+
+        })
+
+
+        };
+
+
+      
+
+
+
+    })
+
+
+    .controller('assignType', function($filter, $sce, ngTableParams, tableService) {
+        //alert('testss')
+
+        this.init = function(){
+            alert('testing')
+        };
+    })
---
+++ routes/unAuthenticatedUsers.js
@@ -0,0 +1,36 @@
+var mysql       = require('mysql');
+
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'FacescSchema'
+});
+
+connection.connect();
+
+module.exports = function (app){
+
+/*connection.query('SELECT * from (password INNER JOIN user on password.userID = user.userID) where user.email=?',['alan'] , function(err, rows, fields) { //WHERE user.email=?',['alan']
+	console.log(rows[0])
+})*/
+/*var randomToken = require('random-token').create('abcdefghijklmnopqrstuvwxzyABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789');
+var token = randomToken(16); // example output → '3ZGErMDCwxTOZYFp' 
+console.log(token)*/
+
+	app.get('/unAuthenticatedUsersAdmin', function(req,res){
+		
+		var Email = req.body.Email;
+		var Password = req.body.Password;
+        
+        connection.query('SELECT * from user where userType=?',[5], function(err, rows, fields) {
+        	if(err){
+        		res.send(err)	
+        	}
+
+        	console.log(rows)
+
+        	res.send(rows);
+	    })
+    })
+}
